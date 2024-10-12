4e95c529f8961b0ab749ba8b6ea0a6f812aa3388
Juan Giusti <jmgiusti91@gmail.com>
2024-10-12 16:35:44 -0300

    feat: stripe checkout session endpoint

---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+
+@Controller('payments')
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('create-payment-session')
+  createPaymentSession(@Body() paymentSessionDto: PaymentSessionDto) {
+    return this.paymentsService.createPaymentSession(paymentSessionDto);
+  }
+
+  @Get('success')
+  success() {
+    return 'success';
+  }
+
+  @Get('cancel')
+  cancel() {
+    return 'cancel';
+  }
+
+  @Post('webhook')
+  async stripeWebhook() {
+    return 'stripeWebhook';
+  }
+}
