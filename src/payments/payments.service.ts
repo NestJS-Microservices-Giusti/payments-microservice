2b5b74dd4f4a99075a313c432a62110202261429
Juan Giusti <jmgiusti91@gmail.com>
2024-10-13 20:13:01 -0300

    feat: env vars and metadata with order id

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config';
 import Stripe from 'stripe';
 import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
-    const { currency, items } = paymentSessionDto;
+    const { currency, items, orderId } = paymentSessionDto;
 
     const lineItems = items.map((item) => ({
       price_data: {
@@ -22,14 +23,47 @@ export class PaymentsService {
     }));
 
     const session = await this.stripe.checkout.sessions.create({
-      // order id
-      payment_intent_data: { metadata: {} },
+      payment_intent_data: {
+        metadata: {
+          orderId,
+        },
+      },
       line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:3003/payments/success',
-      cancel_url: 'http://localhost:3003/payments/cancel',
+      success_url: envs.stripeSuccessUrl,
+      cancel_url: envs.stripeCancelUrl,
     });
 
     return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+    const endpointSecret = envs.stripeEndpointSecret;
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+        const orderId = chargeSucceeded.metadata.orderId;
+
+        console.log(`Charge Succeeded for order ${orderId}`);
+        break;
+      default:
+        console.log(`Event ${event.type} not handled`);
+    }
+
+    return res.status(200).json({ sig });
   }
 }
